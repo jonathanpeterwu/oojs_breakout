ae461beceae6a62787ffbc404453f943451028dc
jonathanpeterwu <jpwu03@yahoo.com>
2014-08-18 21:46:22 -0700

    demo.js

--- README.md
+++ README.md
@@ -6,11 +6,9 @@ Learning Competencies
 - What is good OOJS design?
 - How might OOJS apply to the real world
 - How does OOJS help with MVC javascript
-- WTF is js/oojs/trendy abbreviation?
 
 Steps
-V0 - What is our premise
-V1 - Building our Model (Model/Collection)
+V1 - Building our Model (User Model/Collection)
 V2 - Building our Controller (Github/AJAX/User Creation)
 V3 - Building our View (Rendering Users)
 V4 - Celebrate
--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 
-
-// MODEL
-var User = function(name, username, gravatar, email, repos){
-	this.name = name;
-	this.username = username;
-	this.gravatar = gravatar;
-	this.email = email;
-	this.repos = repos
-}
-
-// COLLECTION
-var Users = []
-
-
-// VIEW
-function renderUser(){
-	var i = Users.length - 1
-	var userTemplate = "<div id='"+ Users[i].name +"'><h1>" + Users[i].name + "</h1><p>" + Users[i].username +"</p><p>" +Users[i].email+ "</p><p>Repos: " + Users[i].repos+ "</p><img src='"+ Users[i].gravatar +"' alt='github image' height='300' width='300'</img> </div>"
-	$('.jumbotron').append(userTemplate)
-}
-
-
 // CONTROLLER
-
 document.addEventListener("DOMContentLoaded", function() {
 	$('.form').on('submit', queryUser)
 });
@@ -49,9 +26,31 @@ function startGithub(username){
 	});
 }
 
+// VIEW
+function renderUser(){
+	var i = Users.length - 1
+	var userTemplate = "<div id='"+ Users[i].name +"'><h1>" + Users[i].name + "</h1><p>" + Users[i].username +"</p><p>" +Users[i].email+ "</p><p>Repos: " + Users[i].repos+ "</p><img src='"+ Users[i].gravatar +"' alt='github image' height='300' width='300'</img> </div>"
+	$('.jumbotron').append(userTemplate)
+}
 
 function initializeUser(result){
 	user = new User(result.name, result.login, result.avatar_url, result.email, result.public_repos)
 	Users.push(user)
 	renderUser()
 }
+
+// MODEL
+var Users = []
+
+var User = function(name, username, gravatar, email, repos){
+	this.name = name;
+	this.username = username;
+	this.gravatar = gravatar;
+	this.email = email;
+	this.repos = repos
+}
+
+
+
+
+
